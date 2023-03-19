8eaa18cf9eaeecdf5f53f0cc1bbe30f83fce206e
ElmarsMozulis <125596330+ElmarsMozulis@users.noreply.github.com>
2023-03-19 18:26:07 +0000

    def build_heap(data, i, swaps):     n = len(data)     min = i     l = 2*i+1     r = 2*i+2
        if l < n and data[l] < data[min]:
            min = l
    
        if r < n and data[r] < data[min]:
            min = r
    
        if i != min:
            swaps.append((i,min))
            data[i],data[min] = data[min],data[i]
            build_heap(data,min,swaps)
        return swaps
    
    def main():
        input_type = input().lower()
    
        if input_type == "i":
            n = int(input())
            data = list(map(int, input().split()))
        elif input_type == "f":
            while True:
                try:
                    file_name = input()
                    with open(f"tests/{file_name}", "r", encoding="utf-8") as f:
                        n = int(f.readline())
                        data = list(map(int, f.readline().split()))
                    break
                except FileNotFoundError:
                    print("File not found, please enter a valid file name")
        else:
            n = 0
            data = []
    
        assert len(data) == n
        swaps = []
        for i in range(n//2,-1,-1):
            swaps = build_heap(data, i, swaps)
    
        print(len(swaps))
        for i, j in swaps:
            print(i, j)
    
    if __name__ == "__main__":
        main()

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,32 @@
 # python3
 
+def heapify(data,n,i,swaps):
+    left = 2*i+1
+    right = 2*i+2
+
+    smallest = i
+    if left<n and data[left] < data[smallest]:
+        smallest = left
+    if right<n and data[right] < data[smallest]:
+        smallest = right
+
+    if smallest !=i:
+        swaps.append((i,smallest))
+        data[i], data[smallest] = data[smallest], data [i]
+        heapify(data,n,smallest,swaps)        
+
+
+
 
 def build_heap(data):
     swaps = []
+    n = len(data)
+
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
-
+    for i in range(n//2,-1,-1):
+        heapify(data,n,i,swaps)
     return swaps
 
 
@@ -17,12 +37,15 @@ def main():
     # first two tests are from keyboard, third test is from a file
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    # input from file
+    filename = int(input("Enter filename: "))
+    with open(filename) as f:
+        n = int(f.readline())
+        data = list(map(int, f.readline().split()))
+    
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    #assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
@@ -34,8 +57,8 @@ def main():
 
     # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for swap in swaps:
+        print(swap[0], swap[1])
 
 
 if __name__ == "__main__":
